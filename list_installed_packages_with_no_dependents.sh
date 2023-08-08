727cef732fd7007be84c12ac31c518350ab123f5
Thomas Grusz <thomasgrusz@ubuntuserver100.b47-homenet>
2023-08-08 22:20:49 +0200

    add new script
    list_installed_packages_with_no_dependents.sh

---
+++ list_installed_packages_with_no_dependents.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Define output filename
+filename=packages_no_dependents.txt
+
+# Write heading to file
+echo "The following packages are not a dependency to any installed package:" > $filename
+
+# Get all installed packages that do not have a 'Priority' of 'required', 'important' or 'standard' and do not have packages that depend on them
+dpkg-query -Wf '${Package} ${Status;-26}${Priority}\n' | sort -b -k5,5 -k1,1 | grep -v 'required\|important\|standard' | grep 'installed' | awk '{ print $1 }' |
+xargs apt-cache rdepends --installed |
+awk '! /Reverse Depends:/ {
+    tp = $0
+    n++
+}
+
+/Reverse Depends:/ {
+    if (n == 1) {
+        print p
+    }
+    n = 0
+    p = tp
+}
+
+END {
+    if (n == 0) {
+        print p
+    }
+}
+' | tee -a $filename
+
+# Remove leading white-space in file
+sed -i '3,$s/\s*//' $filename
